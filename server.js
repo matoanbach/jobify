2d8041d21e731dfdd23b1299282cc91af1ecfd37
toan <bachmatoan2910@gmail.com>
2023-08-04 21:01:03 -0400

    Fix Front-end build command

--- server.js
+++ server.js
@@ -1,72 +1,80 @@
-import 'express-async-errors';
-import * as dotenv from 'dotenv';
+import "express-async-errors";
+
+import * as dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
+import express from "express";
 const app = express();
-import morgan from 'morgan';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-import cloudinary from 'cloudinary';
+import morgan from "morgan";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cloudinary from "cloudinary";
 import helmet from 'helmet';
 import mongoSanitize from 'express-mongo-sanitize';
 
 // routers
-import jobRouter from './routes/jobRouter.js';
-import authRouter from './routes/authRouter.js';
-import userRouter from './routes/userRouter.js';
-// public
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import jobRouter from "./routes/jobRouter.js";
+import authRouter from "./routes/authRouter.js";
+import userRouter from "./routes/userRouter.js";
+//public
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
-// middleware
-import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
-import { authenticateUser } from './middleware/authMiddleware.js';
+//middleware
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { authenticateUser } from "./middleware/authMiddleware.js";
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.CLOUD_API_KEY, 
+  api_secret: process.env.CLOUD_API_SECRET, 
 });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
-app.use(express.static(path.resolve(__dirname, './client/dist')));
+
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
 app.use(mongoSanitize());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("hello world");
 });
 
-app.get('/api/v1/test', (req, res) => {
-  res.json({ msg: 'test route' });
+app.post("/api/v1/test", (req, res) => {
+  const { name } = req.body;
+  res.json({ message: `hello ${name}` });
 });
 
-app.use('/api/v1/jobs', authenticateUser, jobRouter);
-app.use('/api/v1/users', authenticateUser, userRouter);
-app.use('/api/v1/auth', authRouter);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+//job router
+app.get("/api/v1/test", (req, res) => {
+  res.json({ msg: "test route" });
 });
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
+app.use("/api/v1/auth", authRouter);
 
-app.use('*', (req, res) => {
-  res.status(404).json({ msg: 'not found' });
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"))
+})
+
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
 });
 
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5100;
-
 try {
   await mongoose.connect(process.env.MONGO_URL);
   app.listen(port, () => {
-    console.log(`server running on PORT ${port}...`);
+    console.log(`server running on port ${port}...`);
   });
 } catch (error) {
   console.log(error);
--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -1,13 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const createJWT = (payload) => {
-  const token = jwt.sign(payload, process.env.JWT_SERCRET, {
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
   return token;
 };
 
 export const verifyJWT = (token) => {
-  const decoded = jwt.verify(token, process.env.JWT_SERCRET);
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
   return decoded;
 };
